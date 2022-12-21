5d32eafeb2140ba7c5852f3f5d2c4db8b9bbad80
darsh0230 <darshss0230@gmail.com>
2022-12-21 16:21:30 +0530

    don with face selection

--- components/box.jsx
+++ components/box.jsx
@@ -5,83 +5,156 @@ import { STLLoader } from "three/examples/jsm/loaders/STLLoader";
 import { Edges } from "@react-three/drei";
 import * as THREE from "three";
 
-function Box(props) {
+function CustModelLoader({
+  props,
+  modelUrl = "",
+  onhoverColor = [0, 0, 0],
+  modelColor = [0, 1, 1],
+}) {
   const [geometry, setGeometry] = useState();
-  // const [bbox, setBbox] = useState({
-  //   max: { x: 2, y: 2, z: 2 },
-  //   min: { x: 1, y: 1, z: 1 },
-  // });
+  const [geoScale, setGeoScale] = useState(1);
+  const [geoPos, setGeoPos] = useState([0, 0, 0]);
+  const colors = [];
+
+  const [bbox, setBbox] = useState({
+    max: { x: 0, y: 0, z: 0 },
+    min: { x: 0, y: 0, z: 0 },
+  });
 
   useEffect(() => {
     const stlLoader = new STLLoader();
-    stlLoader.load("Clothe clip.STL", (geo) => {
+    stlLoader.load(modelUrl, (geo) => {
       geo.center();
       setGeometry(geo);
 
-      // https://stackoverflow.com/questions/73139301/how-can-i-get-the-size-of-an-object-in-react-three-fiber
-      // geo.computeBoundingBox();
-      // setBbox(geo.boundingBox);
-      // console.log(geo.boundingBox);
+      geo.computeBoundingBox();
+
+      const computeScale =
+        (1 /
+          (Math.abs(Math.min(...geo.boundingBox.max)) +
+            Math.abs(Math.min(...geo.boundingBox.min)))) *
+        3;
+      setGeoScale(computeScale);
+
+      setGeoPos([
+        (geo.boundingBox.max.x - geo.boundingBox.min.x) * 0.5 * computeScale,
+        (geo.boundingBox.max.y - geo.boundingBox.min.y) * 0.5 * computeScale,
+        (geo.boundingBox.max.z - geo.boundingBox.min.z) * 0.5 * computeScale,
+      ]);
+
+      setBbox({
+        max: {
+          x: geo.boundingBox.max.x * computeScale,
+          y: geo.boundingBox.max.y * computeScale,
+          z: geo.boundingBox.max.z * computeScale,
+        },
+        min: {
+          x: geo.boundingBox.min.x * computeScale,
+          y: geo.boundingBox.min.y * computeScale,
+          z: geo.boundingBox.min.z * computeScale,
+        },
+      });
     });
-  }, []);
+  }, [modelUrl]);
 
   return (
     <>
       <mesh
         {...props}
-        onClick={(e) => {
-          // console.log(...e.object.quaternion);
-          const quaternion = new THREE.Quaternion();
+        onUpdate={(e) => {
+          if (JSON.stringify(e.geometry.attributes) !== "{}") {
+            for (let i = 0; i < e.geometry.attributes.position.count; i++) {
+              colors.push(...modelColor);
+            }
 
+            e.geometry.setAttribute(
+              "color",
+              new THREE.BufferAttribute(new Float32Array(colors), 3)
+            );
+          }
+        }}
+        onPointerMove={(e) => {
+          const vertex = new THREE.Vector3();
+          vertex.fromBufferAttribute(
+            e.object.geometry.getAttribute("position"),
+            e.face.a
+          );
+
+          e.object.geometry.setAttribute(
+            "color",
+            new THREE.BufferAttribute(new Float32Array(colors), 3)
+          );
+
+          const { color } = e.object.geometry.attributes;
+
+          color.setX(e.face.a, onhoverColor[0]);
+          color.setY(e.face.a, onhoverColor[1]);
+          color.setZ(e.face.a, onhoverColor[2]);
+
+          color.setX(e.face.b, onhoverColor[0]);
+          color.setY(e.face.b, onhoverColor[1]);
+          color.setZ(e.face.b, onhoverColor[2]);
+
+          color.setX(e.face.c, onhoverColor[0]);
+          color.setY(e.face.c, onhoverColor[1]);
+          color.setZ(e.face.c, onhoverColor[2]);
+
+          e.object.geometry.attributes.color.needsUpdate = true;
+        }}
+        onClick={(e) => {
+          // apply rotation
+          const quaternion = new THREE.Quaternion();
           quaternion.setFromUnitVectors(e.face.normal, e.object.rotation);
 
-          // const quaternion2 = new THREE.Quaternion();
-          // quaternion2.setFromUnitVectors(
-          //   quaternion,
-          //   new THREE.Vector3(0, -1, 0)
-          // );
-
-          // quaternion.multiply(new THREE.Quaternion(0, 0, 1, 0));
-
           e.object.rotation.set(0, 0, 0);
-
           quaternion.setFromUnitVectors(
             e.face.normal,
             new THREE.Vector3(0, -1, 0)
           );
 
-          // console.log(quaternion);
           e.object.applyQuaternion(quaternion);
+
+          e.object.position.set(0, 0, 0);
+
+          // offsetting the rotated object
+          var bbox = new THREE.Box3().setFromObject(e.object);
+          setBbox(bbox);
+
+          e.object.position.set(
+            Math.abs(bbox.min.x),
+            Math.abs(bbox.min.y),
+            Math.abs(bbox.min.z)
+          );
         }}
         geometry={geometry}
-        //TODO: add auto scaling n positioning n also rever this
-        // https://stackoverflow.com/questions/63311362/how-does-threejs-get-the-outermost-size-of-the-model
-        scale={[0.05, 0.05, 0.05]}
-        // rotation-zx={Math.PI / 2}
+        scale={[geoScale, geoScale, geoScale]}
         recieveShadow={true}
-        castShadow={true}>
+        castShadow={true}
+        position={geoPos}>
         {/* <boxBufferGeometry /> */}
-        <meshPhysicalMaterial color={"white"} />
+        <meshPhysicalMaterial vertexColors={true} />
       </mesh>
-      {/* <mesh
+
+      <mesh
         {...props}
-        onClick={(e) => console.log(...e.face.normal)}
-        //TODO: add auto scaling n positioning n also rever this
-        // https://stackoverflow.com/questions/63311362/how-does-threejs-get-the-outermost-size-of-the-model
-        scale={[0.05, 0.05, 0.05]}
         recieveShadow={true}
-        castShadow={true}>
+        castShadow={true}
+        position={[
+          (bbox.max.x - bbox.min.x) * 0.5,
+          (bbox.max.y - bbox.min.y) * 0.5,
+          (bbox.max.z - bbox.min.z) * 0.5,
+        ]}>
         <boxGeometry
           args={[
-            Math.abs(bbox.max.x - bbox.min.x) + 0.5,
-            Math.abs(bbox.max.y - bbox.min.y) + 0.5,
-            Math.abs(bbox.max.z - bbox.min.z) + 0.5,
+            Math.abs(bbox.max.x - bbox.min.x),
+            Math.abs(bbox.max.y - bbox.min.y),
+            Math.abs(bbox.max.z - bbox.min.z),
           ]}
         />
         <meshPhongMaterial color="#ffffff" opacity={0.1} transparent />
         <Edges color={"white"} />
-      </mesh> */}
+      </mesh>
     </>
   );
 }
-export default Box;
+export default CustModelLoader;
--- pages/index.js
+++ pages/index.js
@@ -10,18 +10,7 @@ import { Plane } from "@react-three/drei";
 
 import * as THREE from "three";
 
-export default function Home() {
-  // // https://www.youtube.com/watch?v=CbUhot3K-gc
-  // const pointer = new THREE.Vector2();
-  // const raycaster = new THREE.Raycaster();
-
-  //   const onMouseClick = (event) => {
-  //     pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
-  //     pointer.y = (event.clientY / window.innerHeight) * 2 + 1;
-  //     raycaster.setFromCamera(pointer, test.camera);
-  //   };
-  //   window.addEventListener("mousedown");
-
+export default function CustStlViewer() {
   return (
     <div className={style.scene}>
       <Canvas
@@ -31,8 +20,8 @@ export default function Home() {
           position: [-6, 7, 7],
         }}>
         <ambientLight color={"white"} intensity={0.2} />
-        <LightBulb position={[0, 3, 0]} />
-        <Box rotateX={3} rotateY={0.2} />
+        {/* <LightBulb position={[0, 8, 0]} /> */}
+        <Box modelUrl="cube.stl" />
         {/* <Floor position={[0, -1, 0]} /> */}
         <primitive object={new THREE.AxesHelper(10)} />
         <OrbitControls />
